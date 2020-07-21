0b724fe23b3e6f8a76b1bc4f43535a61656076a9
Sophie <sophiebrown438@gmail.com>
2020-07-21 13:38:31 +0100

    Refactor pass test for json data to router

--- __tests__/indexSpec.js
+++ __tests__/indexSpec.js
@@ -1,29 +1,28 @@
-const router = require('../routes/index')
 const request = require("supertest");
-const express = require('express');
 const app = require('../app');
 
-
-
 describe('Index router', () => {
-  test('it should return data', async () => {
+  const expectedResult = [
+    ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+    ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
+    ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
+    ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
+    ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
+    ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
+    ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
+    ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
+    ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
+    ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
+    ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B']
+  ]
+
+  test('it should return data', async (done) => {
     const res = await request(app)
     .get('/')
     .expect('Content-Type', "application/json; charset=utf-8")
     .expect('Content-Length', '862')
       expect(res.statusCode).toEqual(200)
-      expect(res.body).toEqual([
-        ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-        ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-        ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-        ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-        ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-        ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-        ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-        ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-        ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-        ['B', 'F1', 'F11', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-        ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B']
-      ])
-    })
+      expect(res.body).toEqual(expectedResult)
+      done()
+  })
 })
