c66d340b346abae811849d424f3aab4eb9bd49fa
Rachael <rachaelewins@hotmail.com>
2020-07-27 11:20:24 +0100

    testing output for different codes

--- __tests__/indexSpec.js
+++ __tests__/indexSpec.js
@@ -8,31 +8,11 @@ const addData = imports.addData
 describe('Index router', () => {
   let server;
   let request;
-  const expectedResult = {"level1": [
-  ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-  ['B', 'VS', 'F1', 'F1', 'F1', 'F1', 'I1', 'F1', 'F1', 'F1', 'F1', 'B', 'B', 'B', 'B', 'B'],
-  ['B', 'F1', 'F1', 'B', 'B', 'B', 'B', 'P1', 'B', 'B', 'B', 'F1', 'B', 'B', 'B', 'B'],
-  ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'F1', 'B', 'B', 'B', 'B'],
-  ['B', 'B', 'E', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'F1', 'F1', 'F1', 'F1', 'F1', 'B'],
-  ['B', 'B', 'F1', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'F1', 'B'],
-  ['B', 'B', 'F1', 'B', 'B', 'F1', 'F1', 'F1', 'F1', 'B', 'B', 'B', 'B', 'F1', 'B', 'B'],
-  ['B', 'B', 'F1', 'B', 'B', 'F1', 'B', 'B', 'F1', 'B', 'B', 'B', 'B', 'F1', 'B', 'B'],
-  ['B', 'P3', 'I3', 'B', 'B', 'F1', 'B', 'B', 'F1', 'B', 'F1', 'F1', 'F1', 'I2', 'P2', 'B'],
-  ['B', 'B', 'F1', 'F1', 'F1', 'F1', 'B', 'B', 'F1', 'F1', 'F1', 'B', 'B', 'B', 'B', 'B'],
-  ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B']
-  ], "level2": [
-    ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-    ['B', 'B', 'B', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'F1', 'T', 'P2', 'B', 'B', 'B'],
-    ['B', 'B', 'B', 'F1', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-    ['B', 'B', 'B', 'F1', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-    ['B', 'B', 'B', 'F1', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-    ['B', 'B', 'B', 'PB', 'B', 'B', 'B', 'VS', 'I1', 'F1', 'PB', 'F1', 'PB', 'F1', 'F1', 'B'],
-    ['B', 'B', 'B', 'F1', 'B', 'B', 'B', 'B', 'P1', 'B', 'B', 'B', 'B', 'B', 'F1', 'B'],
-    ['B', 'B', 'F1', 'F1', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'F1', 'B'],
-    ['B', 'B', 'F1', 'B', 'B', 'B', 'B', 'B', 'F1', 'PB', 'F1', 'PB', 'F1', 'PB', 'F1', 'B'],
-    ['B', 'B', 'F1', 'F1', 'PB', 'F1', 'PB', 'F1', 'F1', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
-    ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B']
-  ]}
+  const expectedResult = {"value": "B", "x": 32, "y": 32, "blocked": true}
+  const expectedResultF1 = {"value": "F1", "x": 10, "y": 10, "blocked": false}
+  const expectedResultP1 = {"value": "P1", "x": 15, "y": 15, "blocked": true}
+  const expectedResultOther = {"value": "something", "x": 30, "y": 30, "blocked": false}
+
 
   beforeAll((done) => {
     server = http.createServer(app);
@@ -44,10 +24,10 @@ describe('Index router', () => {
     server.close(done);
   });
 
-  // test('it should return data', async () => {
-  //   const res = await request.get('/');
-  //   expect(res.body).toEqual(expectedResult)
-  // })
+  test('it should return data', async () => {
+    const res = await request.get('/');
+    expect(res.body.level1Data[0][1]).toEqual(expectedResult)
+  })
 
   test("It should response the GET method", () => {
     return supertest(app)
@@ -57,8 +37,16 @@ describe('Index router', () => {
       });
   });
 
-  test("It should return an object from a tile letter", () => {
-    console.log(addData([["B"]]))
-    expect(addData([["B"]])).toEqual([[{"value":"B","x":32,"y":32,"blocked":true}]])
+  test("It should return an object from a tile letter, 'B' ", () => {
+    expect(addData([["B"]])).toEqual([[expectedResult]])
+  })
+  test("It should return an object from a tile letter, 'F1' ", () => {
+    expect(addData([["F1"]])).toEqual([[expectedResultF1]])
+  })
+  test("It should return an object from a tile letter, 'P1' ", () => {
+    expect(addData([["P1"]])).toEqual([[expectedResultP1]])
+  })
+  test("It should return an object from a tile letter, 'other' ", () => {
+    expect(addData([["something"]])).toEqual([[expectedResultOther]])
   })
 });
