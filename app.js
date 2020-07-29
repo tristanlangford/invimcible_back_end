27fa35cbb9e87c98628756ae48f4d6029f962ade
Rachael <rachaelewins@hotmail.com>
2020-07-29 14:24:44 +0100

    access header

--- app.js
+++ app.js
@@ -30,7 +30,10 @@ app.use('/save', saveRouter)
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+})
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
