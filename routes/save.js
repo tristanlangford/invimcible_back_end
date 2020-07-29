e3d7b8651b624c619a7a0cc029abcadf7e9694dd
Tristan Langford <tristanlangford@hotmail.com>
2020-07-29 11:15:44 +0100

    set dbconnection

--- routes/save.js
+++ routes/save.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var databaseConnection = require('../databaseModel')
 
 router.post('/:id', async (req, res) => {
     res.status(200).json( "success" )
+    console.log(req)
+    // databaseConnection.save(req)
     
   });
 
   router.get('/:id', async (req, res) => {
-    res.status(200).json( "success" )
-    
+    databaseConnection.getSave(req.params.id).then(function(save) {
+        res.status(200).json( save )  
+    })  
   });
 
 module.exports = router
